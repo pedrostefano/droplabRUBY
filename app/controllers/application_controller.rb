8ed07e55c1bda0ed92ec0b5944db28d0efcaaed7
pedrostefano <pedrostefano03@gmail.com>
2016-10-20 17:30:24 -0200

    Unsaved files2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(item)
-    stored_location_for(item) || items_path
+  def after_sign_in_path_for(inventory)
+    inventories_path
   end
 end
