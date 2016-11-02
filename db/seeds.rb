85c4c7f1b601ed4d58c29996187d3a3d371583c4
pedrostefano <pedrostefano03@gmail.com>
2016-11-01 22:21:31 -0200

    2 users to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,15 @@
+User.create(email: "pedro@pedro.com",
+                    password: "123456",
+                    password_confirmation: "123456",
+                    first_name: "Pedro",
+                    last_name: "Fernandes")
+puts('Toffalini User created.')
+
+User.create(email: "tkempen2@hotmail.com",
+                    password: "102033",
+                    password_confirmation: "102033",
+                    first_name: "Thiago",
+                    last_name: "Kempen")
+puts('Kempen User created.')
 
 
-10.times do |item|
-  Item.create!(name: SecureRandom.hex(12), quantity: SecureRandom.hex(12), price: SecureRandom.hex(12) )
-end
-puts "10 items created"
